778c67642d840abdab87515f37a984f162caa1f6
pezongomickael67 <pezongomickael67@gmail.com>
2025-02-14 03:17:37 +0100

    home page responsive done

--- frontend/src/component/navbar.jsx
+++ frontend/src/component/navbar.jsx
@@ -1,39 +1,86 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { NavLink } from 'react-router-dom'
 import img from '../constant/images'
 import icons from '../constant/icons'
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
+  const handleShowMenu = () => {
+    setOpen(!open)
+  }
+
   return (
-    <div className='z-10 flex justify-center items-center w-full max-w-[1200px] h-20 rounded-full shadow-sm shadow-black fixed top-20 bg-white transform -translate-x-1/2 -translate-y-1/2 left-1/2'>
-      <div className='w-1/5 h-full flex justify-start items-center pl-10'>
-        <img src={img.logo} alt="logo de devmastermind" className='w-12'/>
+    <div className='w-full bg-transparent px-2 md:px-0 fixed top-12 md:top-20 max-w-[1200px] h-14 md:h-20 -translate-x-1/2 -translate-y-1/2 left-1/2'>
+      <div className='z-10 relative flex justify-between md:justify-center items-center w-full max-w-[1200px] h-14 md:h-20 rounded-full shadow-sm shadow-black bg-white transform '>
+        <div className='w-1/5 h-full md:hidden flex justify-start items-center pl-5 cursor-pointer' onClick={handleShowMenu}  >
+          <icons.IoMenu className='text-3xl'/>
+        </div>
+        <div className='w-1/5 h-full flex justify-start items-center pl-5 md:pl-10'>
+          <img src={img.logo} alt="logo de devmastermind" className='w-8 md:w-12'/>
+        </div>
+        <div className=' hidden w-3/5 h-full md:flex justify-center items-center gap-4'>
+          <NavLink className="w-1/5 flex justify-center items-center font-bold relative group" to="/services">
+              <span>Nos services</span>
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-[75%]"></div>
+          </NavLink>
+          <NavLink to="/offres" className="w-1/5 flex justify-center items-center font-bold relative group">
+              <span>Nos Offres</span>
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-[75%]"></div>
+          </NavLink>
+          <NavLink className="w-1/5 flex justify-center items-center font-bold relative group">
+              <span>Nos réalisations</span>
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></div>
+          </NavLink>
+          <NavLink to="/blog" className="w-1/5 flex justify-center items-center font-bold relative group">
+              <span>Notre blog</span>
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-[75%]"></div>
+          </NavLink>
+          <NavLink className="w-1/5 flex justify-center items-center font-bold relative group">
+              <span>A propos</span>
+              <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-[65%]"></div>
+          </NavLink>
+        </div>
+        <div className='w-[150px] md:w-1/5 h-full justify-end items-center flex pr-2 md:pr-10'>
+          <button className='flex justify-center items-center rounded-full font-bold text-white text-[12px] md:text-xl w-[90%] bg-primary h-12'>Nous contacter <icons.MdOutlineArrowRightAlt className='text-3xl top-1 relative flex' /></button>
+        </div>
+        
       </div>
-      <div className='w-3/5 h-full flex justify-center items-center gap-4'>
-        <NavLink className="w-1/5 flex justify-center items-center font-bold relative group" to="/services">
-            <span>Nos services</span>
-            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-[75%]"></div>
-        </NavLink>
-        <NavLink className="w-1/5 flex justify-center items-center font-bold relative group">
-            <span>Nos Offres</span>
-            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-[75%]"></div>
-        </NavLink>
-        <NavLink className="w-1/5 flex justify-center items-center font-bold relative group">
-            <span>Nos réalisations</span>
-            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-full"></div>
-        </NavLink>
-        <NavLink className="w-1/5 flex justify-center items-center font-bold relative group">
-            <span>Notre blog</span>
-            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-[75%]"></div>
-        </NavLink>
-        <NavLink className="w-1/5 flex justify-center items-center font-bold relative group">
-            <span>A propos</span>
-            <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -translate-y-1/2 w-0 h-[2px] bg-black transition-all duration-300 group-hover:w-[65%]"></div>
-        </NavLink>
-      </div>
-      <div className='w-1/5 h-full justify-end items-center flex pr-10'>
-        <button className='flex justify-center items-center rounded-full font-bold text-white font-xl w-[90%] bg-primary h-12'>Nous contacter <icons.MdOutlineArrowRightAlt className='text-3xl top-1 relative flex' /></button>
-      </div>
+      <div className={'z-10 fixed w-[50%] h-screen flex flex-col items-center bg-white shadow-sm shadow-black -top-5 md:hidden duration-1000 ' + (open ? 'left-0' : '-left-full')}>
+          <div className='absolute top-0 w-full h-10 justify-between flex items-center'>
+            <div className='w-1/5 h-full flex justify-start items-center cursor-pointer' onClick={handleShowMenu}>
+              <icons.IoCloseSharp className='text-3xl'/>
+            </div>
+            <NavLink className='w-1/5 h-full flex justify-start items-center pr-5'>
+              <img src={img.logo} alt="logo de devmastermind" className='w-10'/>
+            </NavLink>
+          </div>
+          <div className='w-full justify-start flex flex-col items-start top-20 relative px-2 gap-10 underline'>
+            <NavLink className="text-lg font-bold flex items-center gap-2" to="/">
+              <icons.GoHomeFill className='font-bold text-2xl' />
+              Accueil
+            </NavLink>
+            <NavLink className="text-lg font-bold flex items-center gap-2" to="/">
+              <icons.MdMiscellaneousServices  className='font-bold text-2xl' />
+              Services
+            </NavLink>
+            <NavLink className="text-lg font-bold flex items-center gap-2" to="/">
+              <icons.PiHandArrowUpBold className='font-bold text-2xl' />
+              Offres
+            </NavLink>
+            <NavLink className="text-lg font-bold flex items-center gap-2" to="/">
+              <icons.RiProjectorFill  className='font-bold text-2xl' />
+              Réalisations
+            </NavLink>
+            <NavLink className="text-lg font-bold flex items-center gap-2" to="/">
+              <icons.SiBlogger className='font-bold text-2xl' />
+              Blog
+            </NavLink>
+            <NavLink className="text-lg font-bold flex items-center gap-2" to="/">
+              <icons.FaMessage className='font-bold text-2xl' />
+              A propos
+            </NavLink>
+          </div>
+        </div>
     </div>
   )
 }
--- frontend/src/constant/icons.js
+++ frontend/src/constant/icons.js
@@ -1,14 +1,19 @@
-import { MdSubject, MdOutlineArrowRightAlt } from "react-icons/md";
+import { MdSubject, MdOutlineArrowRightAlt, MdMiscellaneousServices  } from "react-icons/md";
 import { RiNotificationBadgeFill } from "react-icons/ri";
 import { BiSolidNotification } from "react-icons/bi";
 import { HiArrowLongRight, HiOutlineCube } from "react-icons/hi2";
 import { TbOval } from "react-icons/tb";
 
-import { RiAccountCircleLine } from "react-icons/ri";
+import { RiAccountCircleLine, RiProjectorFill  } from "react-icons/ri";
 import { MdMailOutline } from "react-icons/md";
-import { FaFacebook } from "react-icons/fa";
+import { FaFacebook  } from "react-icons/fa";
 import { FaInstagramSquare } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import { IoMenu, IoCloseSharp, IoHomeOutline   } from "react-icons/io5";
+import { GoHomeFill } from "react-icons/go";
+import { PiHandArrowUpBold } from "react-icons/pi";
+import { SiBlogger } from "react-icons/si";
+import { FaMessage } from "react-icons/fa6";
 
 
 const icons = {
@@ -23,7 +28,16 @@ const icons = {
     RiAccountCircleLine ,
     FaFacebook,
     FaInstagramSquare,
-    FaLinkedin
+    FaLinkedin,
+    IoMenu,
+    IoCloseSharp,
+    IoHomeOutline,
+    GoHomeFill,
+    MdMiscellaneousServices ,
+    PiHandArrowUpBold,
+    RiProjectorFill,
+    SiBlogger,
+    FaMessage 
 }
 
 export default icons
